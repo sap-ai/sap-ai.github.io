cbf867b6f71482cbafa2703e7b8ec2add7c94ada
sap-ai <adriangerleit@gmail.com>
2025-05-31 10:21:52 -0400

    Update tord.js

--- tord.js
+++ tord.js
@@ -1,19 +1,30 @@
 const cars = [1, 2, 3, 4, 5];
-const answer = ["T", "o", "r", "y", "n"];
+let answer = ["T", "o", "r", "y", "n"];
 let trys = 0;
 let wron = 0;
+
 function sub() {
-  trys = trys + 1;
+  trys += 1;
   document.getElementById("t").innerHTML = "Trys: " + trys;
-  for (let x of 5) {
-    if (document.getElementById(cars[x]).innerHTML == answer[x] & document.getElementById(answer[x]).innerHTML != "solved'") {
-      wron = wron + 1;
-      answer[x] = "solved'";
-      document.getElementById(cars[x]).id = "d";
+
+  for (let x = 0; x < 5; x++) {
+    const carEl = document.getElementById(cars[x]);
+    const ans = answer[x];
+
+    if (
+      carEl &&
+      carEl.innerHTML === ans &&
+      ans !== "solved"
+    ) {
+      wron += 1;
+      answer[x] = "solved";
+      carEl.id = "d";
       cars[x] = "d";
     }
-    if (trys < 4 & wron == 5){
-      document.getElementById("t").innerHTML = "you win";
-    }
+  }
+
+  if (trys < 4 && wron === 5) {
+    document.getElementById("t").innerHTML = "You win";
   }
 }
+
